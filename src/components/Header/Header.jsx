3852d0edfa86fdf7e43cef294cf84c96e6ce2a00
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-08-02 23:46:24 +0530

    Template added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+// import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import useStyles from './style'
+import { Search } from '@material-ui/icons';
 
 const Header = () => {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      Header
-    </div>
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h5' className={classes.title}>
+          ExploreMate
+        </Typography>
+
+        <Box display='flex'>
+          <Typography variant='h6' className={classes.title}>
+            Explore new places
+          </Typography>
+
+          {/* <Autocomplete> */}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <Search />
+              </div>
+
+              <InputBase placeholder='Search...' classes={{root: classes.inputRoot, input: classes.inputInput}}/>
+            </div>
+          {/* </Autocomplete> */}
+
+        </Box>
+
+      </Toolbar>
+    </AppBar>
   )
 }
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+// import {
+  //  Paper, Typography,
+  // useMediaQuery } from '@material-ui/core';
+  // import { LocationOnOutlined } from '@material-ui/icons';
+  // import { Rating } from '@material-ui/lab';
+  import useStyles from './style';
 
 const Map = () => {
+
+  const classes = useStyles();
+  // const isMobile = useMediaQuery('(min-width: 600px)');
+  const coordinates = { lat: 0, lng: 0};
+
   return (
-    <div>
-      Map
+    <div className={classes.mapContainer}>
+      <GoogleMapReact bootrapURLKeys={{key: 'AIzaSyBFunsUmQ7N12nT29zMLRFg_srdOdtHSUo'}} 
+        defaultCenter={coordinates} center={coordinates} 
+          defaultZoom={14} margin={[50, 50, 50, 50]} options={''} onChange={''} onChildClick={''}>
+
+      </GoogleMapReact>
     </div>
   )
 }
